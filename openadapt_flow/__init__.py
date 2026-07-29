989c98f2ada81dc58fdba246ebd126555d89cb28
semantic-release <semantic-release>
2026-07-29 13:12:21 +0000

    chore: release 1.26.0

--- openadapt_flow/__init__.py
+++ openadapt_flow/__init__.py
@@ -1,6 +1,6 @@
 """openadapt-flow: record once, compile, replay deterministically, heal on drift."""
 
-__version__ = "1.25.1"
+__version__ = "1.26.0"
 
 from openadapt_flow.ir import (  # noqa: F401
     ActionKind,
